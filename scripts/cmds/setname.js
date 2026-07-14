eede773ff7aaa34f96f31faa641294bb8f003eff
Williams9768 <amir202660@outlook.sa>
2026-07-14 12:51:43 +0100

    Update setname.js

--- scripts/cmds/setname.js
+++ scripts/cmds/setname.js
@@ -1,98 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "setname.json");
+
 async function checkShortCut(nickname, uid, usersData) {
-	try {
-		/\{userName\}/gi.test(nickname) ? nickname = nickname.replace(/\{userName\}/gi, await usersData.getName(uid)) : null;
-		/\{userID\}/gi.test(nickname) ? nickname = nickname.replace(/\{userID\}/gi, uid) : null;
-		return nickname;
-	}
-	catch (e) {
-		return nickname;
-	}
+  try {
+    if (/\{userName\}/gi.test(nickname))
+      nickname = nickname.replace(/\{userName\}/gi, await usersData.getName(uid));
+    if (/\{userID\}/gi.test(nickname))
+      nickname = nickname.replace(/\{userID\}/gi, uid);
+    return nickname;
+  } catch (e) {
+    return nickname;
+  }
+}
+
+let nicknameProtection = {};
+if (fs.existsSync(filePath)) {
+  nicknameProtection = JSON.parse(fs.readFileSync(filePath, "utf8"));
+}
+
+function saveData() {
+  fs.writeFileSync(filePath, JSON.stringify(nicknameProtection, null, 2));
 }
 
 module.exports = {
-	config: {
-		name: "setname",
-		version: "1.5",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Đổi biệt danh của tất cả thành viên trong nhóm chat hoặc những thành viên được tag theo một định dạng",
-			en: "Change nickname of all members in chat or members tagged by a format"
-		},
-		category: "box chat",
-		guide: {
-			vi: {
-				body: "   {pn} <nick name>: thay đổi biệt danh của bản thân"
-					+ "\n   {pn} @tags <nick name>: thay đổi biệt danh của những thành viên được tag"
-					+ "\n   {pn} all <nick name>: thay đổi biệt danh của tất cả thành viên trong nhóm chat"
-					+ "\n\n   Với các shortcut có sẵn:"
-					+ "\n   + {userName}: tên của thành viên"
-					+ "\n   + {userID}: ID của thành viên"
-					+ "\n\n   Ví dụ: (xem ảnh)",
-				attachment: {
-					[`${__dirname}/assets/guide/setname_1.png`]: "https://i.ibb.co/gFh23zb/guide1.png",
-					[`${__dirname}/assets/guide/setname_2.png`]: "https://i.ibb.co/BNWHKgj/guide2.png"
-				}
-			},
-			en: {
-				body: "   {pn} <nick name>: change nickname of yourself"
-					+ "\n   {pn} @tags <nick name>: change nickname of members tagged"
-					+ "\n   {pn} all <nick name>: change nickname of all members in chat"
-					+ "\n\nWith available shortcuts:"
-					+ "\n   + {userName}: name of member"
-					+ "\n   + {userID}: ID of member"
-					+ "\n\n   Example: (see image)",
-				attachment: {
-					[`${__dirname}/assets/guide/setname_1.png`]: "https://i.ibb.co/gFh23zb/guide1.png",
-					[`${__dirname}/assets/guide/setname_2.png`]: "https://i.ibb.co/BNWHKgj/guide2.png"
-				}
-			}
-		}
-	},
+  config: {
+    name: "setname",
+    version: "3.1",
+    author: "Gemini",
+    countDown: 5,
+    role: 1, // أدمن البوت فقط
+    description: {
+      ar: "تبديل كنيات الأعضاء وتفعيل الحماية التلقائية ضد التغيير"
+    },
+    category: "صندوق الدردشة",
+    guide: " {pn} الكل <الكنية>: تبديل كنية الكل وتفعيل الحماية\nمثال: {pn} الكل {userName} | عضو" // <-- هنا صلحتها. وليت string مباشرة
+  },
 
-	langs: {
-		vi: {
-			error: "Đã có lỗi xảy ra, thử tắt tính năng liên kết mời trong nhóm và thử lại sau"
-		},
-		en: {
-			error: "An error has occurred, try turning off the invite link feature in the group and try again later"
-		}
-	},
+  onStart: async function ({ args, message, event, api, usersData }) {
+    const threadID = event.threadID;
+    const nickname = args.slice(1).join(" ");
 
-	onStart: async function ({ args, message, event, api, usersData, getLang }) {
-		const mentions = Object.keys(event.mentions);
-		let uids = [];
-		let nickname = args.join(" ");
+    if (args[0]!== "الكل" && args[0]!== "all")
+      return message.reply("❌ الطريقة: /setname الكل <الكنية>\nمثال: /setname الكل {userName} | عضو");
 
-		if (args[0] === "all" || mentions.includes(event.threadID)) {
-			uids = (await api.getThreadInfo(event.threadID)).participantIDs;
-			nickname = args[0] === "all" ? args.slice(1).join(" ") : nickname.replace(event.mentions[event.threadID], "").trim();
-		}
-		else if (mentions.length) {
-			uids = mentions;
-			const allName = new RegExp(
-				Object.values(event.mentions)
-					.map(name => name.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")) // fix error when name has special characters
-					.join("|")
-				, "g"
-			);
-			nickname = nickname.replace(allName, "").trim();
-		}
-		else {
-			uids = [event.senderID];
-			nickname = nickname.trim();
-		}
+    if (!nickname)
+      return message.reply("❌ خاصك تحدد الكنية اللي بغيتي");
 
-		try {
-			const uid = uids.shift();
-			await api.changeNickname(await checkShortCut(nickname, uid, usersData), event.threadID, uid);
-		}
-		catch (e) {
-			return message.reply(getLang("error"));
-		}
+    const uids = (await api.getThreadInfo(threadID)).participantIDs;
 
-		for (const uid of uids)
-			await api.changeNickname(await checkShortCut(nickname, uid, usersData), event.threadID, uid);
-	}
+    for (const uid of uids) {
+      const finalName = await checkShortCut(nickname, uid, usersData);
+      await api.changeNickname(finalName, threadID, uid);
+      nicknameProtection[threadID] = nicknameProtection[threadID] || {};
+      nicknameProtection[threadID][uid] = finalName;
+    }
+
+    saveData();
+    return message.reply(`✅ تم تبديل ${uids.length} كنية وتفعيل الحماية بنجاح.`);
+  },
+
+  onEvent: async function ({ event, api }) {
+    if (event.logMessageType === "log:user-nickname") {
+      const threadID = event.threadID;
+      const targetUID = event.logMessageData.participant_id;
+      const authorID = event.author;
+      const botID = api.getCurrentUserID();
+
+      if (nicknameProtection[threadID] && nicknameProtection[threadID][targetUID] && authorID!== botID) {
+        const protectedName = nicknameProtection[threadID][targetUID];
+        try {
+          await api.changeNickname(protectedName, threadID, targetUID);
+        } catch (e) {}
+      }
+    }
+  }
 };
